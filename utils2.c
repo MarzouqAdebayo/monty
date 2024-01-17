d9d39836dcbd935b67303ee8aadfef00c47883b9
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-01-17 18:42:45 +0000

    Task 10 - comments fix

--- utils2.c
+++ utils2.c
@@ -50,8 +50,6 @@ void *_realloc(void *ptr, int old_size, int new_size)
  */
 int iscomment(const char *str)
 {
-	int isfirst = 1;
-
 	if (!str || *str == '\0')
 		return (0);
 
@@ -64,4 +62,5 @@ int iscomment(const char *str)
 		else
 			return (0);
 	}
+	return (0);
 }
